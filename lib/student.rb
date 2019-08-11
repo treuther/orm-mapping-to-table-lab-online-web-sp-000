d0750413f36d0e1f5eec28e08bfaf224fb486709
Tracy Reuther <tracyreuther@gmail.com>
2019-08-11 08:58:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,9 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
   
-  def initialize student
+  def initialize (name, grade id = nil)
+    
+  end
   
   def self.create_table
     sql = <<-SQL
